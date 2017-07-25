970465776403e7dea6602cf662a96bbd09c295fd
Øystein Ø. Olsen <fireneslo@gmail.com>
2017-07-25 12:16:28 +0200

    we got stuff on screen!

--- client/index.js
+++ client/index.js
@@ -1,1 +1,12 @@
-console.log("hei")
+import Vue from 'vue'
+import Game from './components/game/game.vue'
+
+const app = document.createElement('main')
+
+
+document.body.appendChild(app)
+
+new Vue({ // eslint-disable-line no-new
+  el: app,
+  render: (h) => h(Game)
+})
